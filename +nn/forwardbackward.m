53e4c76ff68bcaf6a5406d19554b5ddc4b9b6a01
Jerry Lin <jerry.net@gmail.com>
2015-05-12 23:01:45 +0800

    Make this function runs slightly faster
    About 5%, compared to last version.

--- +nn/forwardbackward.m
+++ +nn/forwardbackward.m
@@ -42,51 +42,51 @@ opts = nn.utils.vararginHelper(opts, varargin);
 n = numel(net.layers) ;
 
 if (nargin <= 2) || isempty(dzdy)
-  doder = false ;
+    doder = false ;
 else
-  doder = true ;
+    doder = true ;
 end
 
 if nargin <= 3 || isempty(res)
-  res.blob  = num2cell(zeros(1, numel(net.blobNames), 'single'));
-  res.dzdx  = num2cell(zeros(1, numel(net.blobNames), 'single')); % each cell contains another cell, and the inner cell's length is respected to the number of bottoms that a layer accepts
-  res.dzdw  = num2cell(zeros(1, numel(net.weightsNames), 'single')); % Each dzdw{w} corresponds to a net.weights{w}, no separate dzdw for each layer
-  res.dzdwVisited = false(size(res.dzdw));
-  res.time  = zeros(1,n+1);
-  res.backwardTime = zeros(1,n+1);
+    res.blob  = num2cell(zeros(1, numel(net.blobNames), 'single'));
+    res.dzdx  = num2cell(zeros(1, numel(net.blobNames), 'single')); % each cell contains another cell, and the inner cell's length is respected to the number of bottoms that a layer accepts
+    res.dzdw  = num2cell(zeros(1, numel(net.weightsNames), 'single')); % Each dzdw{w} corresponds to a net.weights{w}, no separate dzdw for each layer
+    res.dzdwVisited = false(size(res.dzdw));
+    res.time  = zeros(1,n+1);
+    res.backwardTime = zeros(1,n+1);
 
 end
 
 for i = fieldnames(x)'
-  name2Ind = net.blobNamesIndex.(i{1});
-  res.blob{name2Ind} = x.(i{1}); %Because x is a structure, eg. x = struct('data',[],'label',[])
+    name2Ind = net.blobNamesIndex.(i{1});
+    res.blob{name2Ind} = x.(i{1}); %Because x is a structure, eg. x = struct('data',[],'label',[])
 end
 
 for i=1:n
-  l = net.layers{i} ;
-  forwardBegin = tic ;
-
-  [topBlob, weightUpdate] = net.layerobjs{i}.forward(opts, l, net.weights(l.weights), res.blob(l.bottom));
-  res.blob(l.top) = topBlob; % if a layer don't generate output, it still should fill topBlob as {[],[],...}
-
-  if ~isempty(weightUpdate)
-    net.weights(l.weights(weightUpdate{1})) = weightUpdate{2};
-  end
-
-  % optionally forget intermediate results
-  forget = opts.conserveMemory ;
-  forget = forget & (~doder || strcmp(l.type, 'relu')) ;
-  forget = forget & ~net.layerobjs{i}.generateLoss ;
-  forget = forget & (~isfield(l, 'rememberOutput') || ~l.rememberOutput) ;
-  if forget
-      res.blob(l.top) = {[]} ;
-  end
-  if opts.gpuMode && opts.sync
-    % This should make things slower, but on MATLAB 2014a it is necessary
-    % for any decent performance.
-    wait(gpuDevice) ;
-  end
-  res.time(i) = toc(forwardBegin) ;
+    l = net.layers{i} ;
+    forwardBegin = tic ;
+  
+    [topBlob, weightUpdate] = net.layerobjs{i}.forward(opts, l, net.weights(l.weights), res.blob(l.bottom));
+    res.blob(l.top) = topBlob; % if a layer don't generate output, it still should fill topBlob as {[],[],...}
+  
+    if ~isempty(weightUpdate)
+        net.weights(l.weights(weightUpdate{1})) = weightUpdate{2};
+    end
+  
+    % optionally forget intermediate results
+    forget = opts.conserveMemory ;
+    forget = forget & (~doder || strcmp(l.type, 'relu')) ;
+    forget = forget & ~net.layerobjs{i}.generateLoss ;
+    forget = forget & (~isfield(l, 'rememberOutput') || ~l.rememberOutput) ;
+    if forget
+        res.blob(l.top) = {[]} ;
+    end
+    if opts.gpuMode && opts.sync
+        % This should make things slower, but on MATLAB 2014a it is necessary
+        % for any decent performance.
+        wait(gpuDevice) ;
+    end
+    res.time(i) = toc(forwardBegin) ;
 end
 
 
@@ -102,31 +102,32 @@ if doder
 
     [tmpdzdx, tmpdzdw] = net.layerobjs{i}.backward(opts, l, net.weights(l.weights), res.blob(l.bottom), res.dzdx(l.top));
 
-    for b = 1:numel(l.bottom)
-      if ~isempty(tmpdzdx{b})
-          if opts.accumulate
-              res.dzdx{l.bottom(b)} = res.dzdx{l.bottom(b)} + tmpdzdx{b};
-          else
-              res.dzdx{l.bottom(b)} = tmpdzdx{b};
-          end
-      end
+    dzdxEmpty = ~cellfun('isempty', tmpdzdx);
+    if opts.accumulate
+        for b = dzdxEmpty
+            res.dzdx{l.bottom(b)} = res.dzdx{l.bottom(b)} + tmpdzdx{b};
+        end
+    else
+        res.dzdx(l.bottom(dzdxEmpty)) = tmpdzdx(dzdxEmpty);
     end
-    for w = 1:numel(l.weights) %numel([]) = 0
-      if ~isempty(tmpdzdw{w})
-          if  opts.accumulate || res.dzdwVisited(w)
-            res.dzdw{l.weights(w)} = res.dzdw{l.weights(w)} + tmpdzdw{w};
-          else
-            res.dzdw{l.weights(w)} = tmpdzdw{w};
-          end
-          res.dzdwVisited(l.weights(w)) = true;
-      end
+    
+
+    dzdwEmpty = ~cellfun('isempty', tmpdzdw);
+
+    dzdwEmpty1 = dzdwEmpty & (res.dzdwVisited(l.weights(dzdwEmpty)) | opts.accumulate);
+    for w = find(dzdwEmpty1)
+      res.dzdw{l.weights(w)} = res.dzdw{l.weights(w)} + tmpdzdw{w};
     end
+    %res.dzdw(l.weights(dzdwEmpty1)) = cellfun(@plus, res.dzdw(l.weights(dzdwEmpty1)), tmpdzdw(dzdwEmpty1), 'UniformOutput', false);
+    res.dzdw(l.weights(dzdwEmpty)) = tmpdzdw(dzdwEmpty);
+
+    res.dzdwVisited(l.weights(dzdwEmpty)) = true;
 
     if opts.conserveMemory %delete used dzdx{top}, no need to consider loss or accuracy, because der(loss)=1, and accuracy has no backward computation
-      res.dzdx(l.top) = {[]} ;
+        res.dzdx(l.top) = {[]} ;
     end
     if opts.gpuMode && opts.sync
-      wait(gpuDevice) ;
+        wait(gpuDevice) ;
     end
     res.backwardTime(i) = toc(backwardBegin) ;
 
