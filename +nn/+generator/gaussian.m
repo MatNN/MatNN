9b195af505175810b955c6cb10512fd7f2973ad3
Jerry Lin <jerry.net@gmail.com>
2015-06-11 10:33:57 +0800

    Change gaussian generator default std to 1

--- +nn/+generator/gaussian.m
+++ +nn/+generator/gaussian.m
@@ -4,7 +4,7 @@ function res = gaussian(dimensionVector, param)
 %  mean
 %  std
     default_param.mean = 0;
-    default_param.std = 0;
+    default_param.std = 1;
     p = nn.utils.vararginHelper(default_param, param);
     res = randn(dimensionVector, 'single')*p.std+p.mean;
 end
